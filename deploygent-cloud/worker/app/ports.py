4e32afc1ea309db77153c45ac4560afdb01ec8a1
prerans <preran248@gmail.com>
2026-07-26 01:44:58 +0530

    its finally time :)

--- deploygent-cloud/worker/app/ports.py
+++ deploygent-cloud/worker/app/ports.py
@@ -1,12 +1,16 @@
 import socket
 
+START_PORT = 9000
+END_PORT = 9999
+
 
 def get_free_port():
+    for port in range(START_PORT, END_PORT + 1):
+        with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as sock:
+            try:
+                sock.bind(("0.0.0.0", port))
+                return port
+            except OSError:
+                continue
 
-    with socket.socket() as sock:
-
-        sock.bind(
-            ("",0)
-        )
-
-        return sock.getsockname()[1]
+    raise RuntimeError("No free ports available")
