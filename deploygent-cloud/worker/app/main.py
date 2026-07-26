f876886905382da52c8b4d85095c54e693fd4ae2
prerans <preran248@gmail.com>
2026-07-26 16:42:05 +0530

    added routes.ts

--- deploygent-cloud/worker/app/main.py
+++ deploygent-cloud/worker/app/main.py
@@ -16,6 +16,7 @@ app.add_middleware(
     CORSMiddleware,
     allow_origins=[
         "http://localhost:3000",
+        "https://agents-sdk-seven.vercel.app"
             # Next.js
     ],
     allow_credentials=True,
